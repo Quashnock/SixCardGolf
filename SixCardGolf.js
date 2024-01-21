fa574e0b309aa50b3790ded6d33409c978684d7f
Quashnock <aaquashnock@gmail.com>
2024-01-20 18:27:47 -0800

    CommitMessage

--- SixCardGolf.js
+++ SixCardGolf.js
@@ -19,8 +19,13 @@ localStorage.setItem('card6Points', 0)
 localStorage.setItem('drawPileFace', 'Assets/EmptyCard.png')
 localStorage.setItem('drawPilePoints', 0)
 
+localStorage.setItem('discardPileFace0', 'Assets/EmptyCard.png')
+localStorage.setItem('discardPilePoints0', 0)
+
 localStorage.setItem('pregameFlips', 0);
+localStorage.setItem('discardPileHeight', 0)
 localStorage.setItem('drawPilePickUp', 'N');
+localStorage.setItem('discardPilePickUp', 'N')
 
 function card1Click(){
     if (localStorage.getItem('card1Face') == 'Assets/EmptyCard.png' && parseInt(localStorage.getItem('pregameFlips')) < 2){
@@ -28,6 +33,9 @@ function card1Click(){
     }
     else if(localStorage.getItem('drawPilePickUp') == 'Y'){
         drawPilePlace('.cardSpace1', 'card1Face', 'card1Points','card1Click()');
+    }
+    else if(localStorage.getItem('discardPilePickUp') == 'Y'){
+        discardPilePlace('.cardSpace1', 'card1Face', 'card1Points','card1Click()');
     }
 }
 function card2Click(){
@@ -37,6 +45,9 @@ function card2Click(){
     else if(localStorage.getItem('drawPilePickUp') == 'Y'){
         drawPilePlace('.cardSpace2', 'card2Face', 'card2Points','card2Click()');
     }
+    else if(localStorage.getItem('discardPilePickUp') == 'Y'){
+        discardPilePlace('.cardSpace2', 'card2Face', 'card2Points','card2Click()');
+    }
 }
 function card3Click(){
     if (localStorage.getItem('card3Face') == 'Assets/EmptyCard.png' && parseInt(localStorage.getItem('pregameFlips')) < 2){
@@ -44,6 +55,9 @@ function card3Click(){
     }
     else if(localStorage.getItem('drawPilePickUp') == 'Y'){
         drawPilePlace('.cardSpace3', 'card3Face', 'card3Points','card3Click()');
+    }
+    else if(localStorage.getItem('discardPilePickUp') == 'Y'){
+        discardPilePlace('.cardSpace3', 'card3Face', 'card3Points','card3Click()');
     }
 }
 function card4Click(){
@@ -53,6 +67,9 @@ function card4Click(){
     else if(localStorage.getItem('drawPilePickUp') == 'Y'){
         drawPilePlace('.cardSpace4', 'card4Face', 'card4Points','card4Click()');
     }
+    else if(localStorage.getItem('discardPilePickUp') == 'Y'){
+        discardPilePlace('.cardSpace4', 'card4Face', 'card4Points','card4Click()');
+    }
 }
 function card5Click(){
     if (localStorage.getItem('card5Face') == 'Assets/EmptyCard.png' && parseInt(localStorage.getItem('pregameFlips')) < 2){
@@ -60,6 +77,9 @@ function card5Click(){
     }
     else if(localStorage.getItem('drawPilePickUp') == 'Y'){
         drawPilePlace('.cardSpace5', 'card5Face', 'card5Points','card5Click()');
+    }
+    else if(localStorage.getItem('discardPilePickUp') == 'Y'){
+        discardPilePlace('.cardSpace5', 'card5Face', 'card5Points','card5Click()');
     }
 }
 function card6Click(){
@@ -69,12 +89,44 @@ function card6Click(){
     else if(localStorage.getItem('drawPilePickUp') == 'Y'){
         drawPilePlace('.cardSpace6', 'card6Face', 'card6Points','card6Click()');
     }
+    else if(localStorage.getItem('discardPilePickUp') == 'Y'){
+        discardPilePlace('.cardSpace6', 'card6Face', 'card6Points','card6Click()');
+    }
 }
 
 function drawPileClick(){
-    if (localStorage.getItem('drawPileFace') == 'Assets/EmptyCard.png' && localStorage.getItem('pregameFlips') >= 2){
+    if (localStorage.getItem('drawPileFace') == 'Assets/EmptyCard.png' && localStorage.getItem('pregameFlips') >= 2 && localStorage.getItem('discardPilePickUp') == 'N'){
         localStorage.setItem('drawPilePickUp', 'Y');
         emptyCardClick('.drawPile', 'drawPileFace', 'drawPilePoints', 'drawPileClick()');
+    }
+}
+function discardPileClick(){
+    if (localStorage.getItem('drawPilePickUp') == 'Y'){
+
+        localStorage.setItem('drawPilePickUp', 'N');
+        localStorage.setItem('discardPileHeight', (parseInt(localStorage.getItem('discardPileHeight')) + 1));
+
+        let container = document.querySelector('.discardPile');
+        let newElement = document.createElement('img');
+        newElement.setAttribute('src', localStorage.getItem('drawPileFace'))
+        newElement.setAttribute('onClick', 'discardPileClick()');
+        container.innerHTML = '';
+        container.appendChild(newElement);
+        localStorage.setItem('discardPileFace' +  localStorage.getItem('discardPileHeight'), localStorage.getItem('drawPileFace'));
+        localStorage.setItem('discardPilePoints' + localStorage.getItem('discardPileHeight'), localStorage.getItem('drawPilePoints'));
+
+        container = document.querySelector('.drawPile');
+        newElement = document.createElement('img');
+        newElement.setAttribute('src', 'Assets/EmptyCard.png');
+        newElement.setAttribute('onClick', 'drawPileClick()');
+        container.innerHTML = '';
+        container.appendChild(newElement);
+        localStorage.setItem('drawPileFace', 'Assets/EmptyCard.png');
+        localStorage.setItem('drawPilePoints', 0);
+        checkPoints(); 
+    }
+    else if(localStorage.getItem('discardPileHeight') > 0 && localStorage.getItem('pregameFlips') >= 2){
+        localStorage.setItem('discardPilePickUp', 'Y');
     }
 }
 
@@ -98,8 +150,28 @@ function emptyCardClick(field, face, points, functionName){
 }
 function drawPilePlace(field, face, points, functionName){
 
-    let container = document.querySelector(field);
+    localStorage.setItem('drawPilePickUp', 'N');
+    localStorage.setItem('discardPileHeight', (parseInt(localStorage.getItem('discardPileHeight')) + 1));
+
+    let container = document.querySelector('.discardPile');
     let newElement = document.createElement('img');
+    newElement.setAttribute('onClick', 'discardPileClick()')
+    container.innerHTML = '';
+    if (localStorage.getItem(face) ==  'Assets/EmptyCard.png'){
+        const randomCard = getRandomCard();
+        newElement.setAttribute('src', randomCard.face);
+        localStorage.setItem('discardPileFace' +  localStorage.getItem('discardPileHeight'), randomCard.face)
+        localStorage.setItem('discardPilePoints' + localStorage.getItem('discardPileHeight'), randomCard.points)
+    }
+    else{
+        newElement.setAttribute('src', localStorage.getItem(face));
+        localStorage.setItem('discardPileFace' +  localStorage.getItem('discardPileHeight'), localStorage.getItem(face));
+        localStorage.setItem('discardPilePoints' + localStorage.getItem('discardPileHeight'), localStorage.getItem(points));
+    }
+    container.appendChild(newElement);
+
+    container = document.querySelector(field);
+    newElement = document.createElement('img');
     newElement.setAttribute('src', localStorage.getItem('drawPileFace'));
     newElement.setAttribute('onClick', functionName);
     container.innerHTML = '';
@@ -116,6 +188,31 @@ function drawPilePlace(field, face, points, functionName){
     localStorage.setItem('drawPileFace', 'Assets/EmptyCard.png');
     localStorage.setItem('drawPilePoints', 0);
     checkPoints(); 
+}
+function discardPilePlace(field, face, points, functionName){
+
+    localStorage.setItem('discardPilePickUp', 'N')
+
+    let container = document.querySelector(field);
+    let newElement = document.createElement('img');
+    newElement.setAttribute('src', localStorage.getItem('discardPileFace' + localStorage.getItem('discardPileHeight')));
+    newElement.setAttribute('onClick', functionName);
+    container.innerHTML = '';
+    container.appendChild(newElement);
+    localStorage.setItem(face, localStorage.getItem('discardPileFace' + localStorage.getItem('discardPileHeight')));
+    localStorage.setItem(points, localStorage.getItem('discardPilePoints' + localStorage.getItem('discardPileHeight')));
+    
+    localStorage.removeItem('discardPileFace' + localStorage.getItem('discardPileHeight'));
+    localStorage.removeItem('discardPilePoints' + localStorage.getItem('discardPileHeight'));
+    localStorage.setItem('discardPileHeight', parseInt(localStorage.getItem('discardPileHeight'))-1);
+
+    container = document.querySelector('.discardPile');
+    newElement = document.createElement('img');
+    newElement.setAttribute('src', localStorage.getItem('discardPileFace' + localStorage.getItem('discardPileHeight')));
+    newElement.setAttribute('onClick', 'discardPileClick()');
+    container.innerHTML = '';
+    container.appendChild(newElement);
+    checkPoints();
 }
 
 function checkPoints(){
